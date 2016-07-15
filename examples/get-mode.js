ef9b8f04b54270d19211bf0a096c250a0c391c8f
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-14 22:17:26 -0700

    chore: Split ET312 into base, serial, emulator

--- examples/get-mode.js
+++ examples/get-mode.js
@@ -1,35 +1,34 @@
 'use strict';
-const SerialPort = require('serialport');
 const buttshock = require('buttshock');
 const async = require('marcosc-async');
-const opts = {
-  baudrate: 19200,
-  // If only we had a viable parser to use
-  parser: SerialPort.parsers.raw
-};
+const e = new buttshock.ET312Serial('/dev/ttyUSB0');
 
-const port = new SerialPort('/dev/ttyUSB0', opts);
-
-port.on('open', function() {
-  async.task(function*() {
-    const e = new buttshock.et312(port);
-    try {
-      yield e.handshake();
-      console.log('handshake succeeded!');
-    } catch (err) {
-      console.log('handshake failed:', err);
-      return port.close();
-    }
-    try {
-      yield e.keyExchange();
-      console.log('Box key: ' + e._key);
-      let mode = yield e.readMode();
-      console.log('Box mode: ' + mode);
-      yield e.resetKey();
-    } catch (err) {
-      console.log('key exchange failed!', err);
-    } finally {
-      return port.close();
-    }
-  });
+async.task(function*() {
+  try {
+    yield e.open();
+  } catch (err) {
+    console.log("Cannot open port!");
+    return;
+  }
+  try {
+    yield e.handshake();
+    console.log('handshake succeeded!');
+  } catch (err) {
+    console.log('handshake failed:', err);
+    yield e.close();
+    return;
+  }
+  try {
+    yield e.keyExchange();
+    console.log('Box key: ' + e._key);
+    let mode = yield e.readMode();
+    console.log('Box mode: ' + mode);
+    yield e.resetKey();
+  } catch (err) {
+    console.log('key exchange failed!', err);
+  } finally {
+    yield e.close();
+    return;
+  }
 });
+
